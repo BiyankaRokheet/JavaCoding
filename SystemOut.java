235daa20d7d553da21aa7238e5b832e93bda5333
BiyankaRokheet <76637386+BiyankaRokheet@users.noreply.github.com>
2020-12-26 21:17:17 -0600

    Create SystemOut.java

---
+++ SystemOut.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class SystemOut
+{
+  
+    
+    public static void main (String[] args) 
+    {
+        HashMap<Integer, String> hm = new HashMap <> ();
+        hm.put(10, "Biyanka");
+        hm.put(15, "Srinika");
+        hm.put(29, "Stuti");
+        hm.put(32, "Srijon");
+        hm.put(120, "Bivash");
+        System.out.println("HelloWorld");
+        System.out.println("The roll number 29 has the name " + hm.get(29));
+    }
+   
+}
